ea6356e4007bed355e1815bf64b51c1fcbc269f5
Johann Poon <johann_poon@epam.com>
2025-05-20 10:50:15 +0800

    Added modified date field in entity

--- src/main/java/org/example/Message.java
+++ src/main/java/org/example/Message.java
@@ -2,10 +2,30 @@ package org.example;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import org.hibernate.envers.Audited;
+
+import java.time.Instant;
 
 @Entity
 @Audited
 public class Message extends PanacheEntity {
     public String message;
+
+    private Instant modifiedDate;
+
+    @PrePersist
+    @PreUpdate
+    public void setModifiedDate() {
+        this.modifiedDate = Instant.now(); // Set current timestamp
+    }
+
+    // Getters and Setters for MODIFIED_DATE
+    public Instant getModifiedDate() {
+        return modifiedDate;
+    }
+    public void setModifiedDate(Instant modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
